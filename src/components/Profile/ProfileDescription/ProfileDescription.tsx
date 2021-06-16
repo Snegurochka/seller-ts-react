a0a3570d253a7ed7f1d13edfa14e94da0d105268
SnegurkaWs <snegurka@web-esse.ru>
2021-06-16 21:54:11 +0500

    added description component

--- src/components/Profile/ProfileDescription/ProfileDescription.tsx
+++ src/components/Profile/ProfileDescription/ProfileDescription.tsx
@@ -1,9 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
 type PropsType = {
     description: string,
-    updateSellerDescription: () => void
+    updateSellerDescription: (description: string) => void
 }
 
-const ProfileDescription: React.FC<PropsType> = ({ description, updateSellerDescription }) => (<div>fdsfsd</div>
-)
+const ProfileDescription: React.FC<PropsType> = ({ description, updateSellerDescription }) => {
+    const [editMode, setEditMode] = useState(false);
+    const [newDescription, setDescription] = useState(description);
+
+    useEffect( () => {
+        setDescription(description)
+    }, [description]);
+
+    const enableEditMode = () => {
+        setEditMode(true);
+    }
+
+    const offEditMode = () => {
+        setEditMode(false);
+        updateSellerDescription(newDescription);
+    }
+
+    const changeHandler = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescription(evt.currentTarget.value);
+    }
+
+    return (
+        <div className="seller__description">
+            {!editMode
+                ? <div onClick={enableEditMode}>{description}</div>
+                : <textarea onBlur={offEditMode} value={newDescription} onChange={changeHandler} >{description}</textarea>
+            }
+        </div>
+    );
+}
 
 export default ProfileDescription;
