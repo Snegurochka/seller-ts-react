503a5181cb2797d669cdf4b44e1cf7edce25716d
SnegurkaWs <snegurka@web-esse.ru>
2021-10-11 11:04:13 +0500

    added chart Component and Recharts lib

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Seller dashboard
 
+Pages:
+- Dashboard
+
 Technologies used: 
 - React, React Hooks, Custom Hooks, 
 - Redux,
 - TypeScript, JS, 
+- CSS Modules + SCSS for layout,
 - Recharts - charting library built on React components
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 
+import Chart from './../Chart/Chart';
+
 const Dashboard: React.FC = () => {
     return (
         <main>
             <section className={styles.overview}>
                 <header>Dashboard</header>
-                <div>
-                    <p className={styles.dataRange}>01-26 November, 2021</p>
-                </div>
+                <Chart />
             </section>
         </main>
     )
