49823e5919ce15b38b183556a62e843739e05300
SnegurkaWs <snegurka@web-esse.ru>
2021-10-11 11:49:11 +0500

    added Layout component-container

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 
+import Layout from './../Layout/Layout';
 import Chart from './../Chart/Chart';
 import BlockOerview from './../BlockOerview/BlockOerview';
 
 const Dashboard: React.FC = () => {
     return (
-        <main>
-            <section className={styles.overview}>
-                <header>Dashboard</header>
-                <Chart />
+        <Layout header='Dashboard' classPage='dashboard'>
+            <div className={styles.overview}>
+            <Chart />
 
-                <BlockOerview header='Today'>
-                    text
-                </BlockOerview>
-            </section>
-        </main>
+            <BlockOerview header='Today'>
+                text
+            </BlockOerview>
+            </div>
+        </Layout>
     )
 }
 
